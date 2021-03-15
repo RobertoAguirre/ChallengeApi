92263d0eb5ada4e54f01d7ec9cea8431592f93b9
roberto aguirre <roberto.aguirre.rodriguez@gmail.com>
2021-03-14 20:03:40 -0700

    Code coverage con prueba hello msg cambiado

--- test/api.js
+++ test/api.js
@@ -1,9 +1,9 @@
 
 const api = require('../server.js');
 
-describe("Endpoint for testing api execution", function() {
+describe("Endpoint for testing the greeting function", function() {
     // specification code
-    describe("Must return Hello when visited", function() {
+    describe("Sends a hello message with a name", function() {
         // specification for RGB to HEX converter
         it("literally just says hello", function() {
             var _hello = api.greetings('roberto');
